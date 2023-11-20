352e95439780fe80a065de22d277343c1a2499c2
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-20 11:55:03 +0100

    modifying index and first page Head and title

--- pages/searchProduct.js
+++ pages/searchProduct.js
@@ -1,8 +1,18 @@
+import Head from 'next/head'
 import React from 'react'
 
 function searchProduct() {
   return (
+    <>
+    {/* we can add titles to every single page we create by importing Head... title etc... */}
+    <Head>
+        <title>
+            search
+        </title>
+    </Head>
     <div>searchProduct</div>
+    </>
+
   )
 }
 
