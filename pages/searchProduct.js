0e2792ec9645c9f6b63cba3efdfd363c8068f85b
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-20 12:53:54 +0100

    playing around with env variables visibility

--- pages/searchProduct.js
+++ pages/searchProduct.js
@@ -2,15 +2,20 @@ import Head from 'next/head'
 import React from 'react'
 
 function searchProduct() {
+    console.log(process.env.NEXT_PUBLIC_SECRET);
+    //this one below will be only visisble in terminal, but undefined in console!
+    console.log(process.env.SECRET);
   return (
     <>
     {/* we can add titles to every single page we create by importing Head... title etc... */}
     <Head>
         <title>
-            search
+            search product
         </title>
     </Head>
     <div>searchProduct</div>
+
+
     </>
 
   )
