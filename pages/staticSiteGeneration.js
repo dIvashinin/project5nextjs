25375855e7d36e95172a3e0b247f3897e925bffa
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-22 11:18:28 +0100

    displaying a random product as static site generation

--- pages/staticSiteGeneration.js
+++ pages/staticSiteGeneration.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Head from "next/head";
 
-function staticSiteGeneration() {
+// here the thing is we will get an item always the same (we specify
+// this item once we deploy our project
+// unless we use some time specific ,which comes later
+export const getStaticProps = async () => {
+const randomId =Math.floor(Math.random()*19);
+const response = await fetch (`https://fakestoreapi.com/products/${randomId}`);
+const result = await response.json();
+
+return{
+   props: {product:result},  
+};
+};
+
+//we receive here in our function below props from getStaticProps
+function staticSiteGeneration({product}) {
   return (
     <div>
         <Head>
@@ -8,8 +23,12 @@ function staticSiteGeneration() {
         </Head>
         <h1>check a random product every time</h1>
         <h3>this page is rendered already in the server</h3>
+        <div>
+          <p>{product.description}</p>
+          <p>{product.title}</p>
+        </div>
         </div>
   )
 }
 
-export default staticSiteGeneration
+export default staticSiteGeneration;
