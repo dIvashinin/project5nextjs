425dff048b051ed1435e79b86ea72de1ce302285
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-22 16:13:46 +0100

    styling of navbar, position fixed, width 100%

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import styles from '../styles/styles.css'
+
 // import { Inter } from "next/font/google";
 
 // const inter = Inter(); 
