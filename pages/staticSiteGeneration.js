5fe7202d6b29bcf764a52452b7c5c165ee3a8a84
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-22 10:54:26 +0100

    creating static site generation page

---
+++ pages/staticSiteGeneration.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function staticSiteGeneration() {
+  return (
+    <div>
+        <Head>
+            <title key="title">Fake store: random product</title>
+        </Head>
+        <h1>check a random product every time</h1>
+        <h3>this page is rendered already in the server</h3>
+        </div>
+  )
+}
+
+export default staticSiteGeneration
