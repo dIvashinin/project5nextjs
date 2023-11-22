47dd4f05a7a09c11bd8f16380b590da9f5870f2a
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-22 10:38:09 +0100

    displaying 1 item as pre-rendered in the server

--- pages/serverSideRender.js
+++ pages/serverSideRender.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-//here go next functio getServerSideProps. we check the documentation
+//here goes next function getServerSideProps, which gives us a possibility 
+// to pre-render stuff and display it without any useState useEffect as before
+// we check the documentation
 export const getServerSideProps = async () => {
 const response = await fetch ("https://fakestoreapi.com/products/1");
 const result = await response.json();
 // this console.log is only happening in our server
 // client doesn't have access to here, only to what i am returning here
-console.log("where am I???");
+console.log("this is visible only in terminal");
 return {
   props: {product: result},
 };
@@ -15,6 +17,7 @@ return {
 function serverSideRender({product}) {
   // this console.log is being displayed in both: server&client
   console.log('product in my client :>> ', product);
+  console.log('this one is visible in both - terminal and browser');
   return (
     <div>
         
