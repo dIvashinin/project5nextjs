92a0ee021d3e3f005787e0124275d16ca5e33b1e
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-23 14:52:01 +0100

    handle functions added in register

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,27 @@
 import React from 'react'
+//need to check the imports, because they can be done automatically wrong
+import { useState } from 'react';
 
 function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        console.log('email :>> ', email);
+    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+        console.log('password :>> ', password);
+    };
+
   return (
     <div>
     <h2>Register</h2>
     <form action="">
-        <input type="text" id="email"/>
+        <input type="text" id="email" onChange={handleEmailChange}/>
         <label htmlFor="email" placeholder="email">email</label>
-        <input type="password" id="password"/>
+        <input type="password" id="password"onChange={handlePasswordChange}/>
         <label htmlFor="password" placeholder="password">password</label>
     </form>
     </div>
