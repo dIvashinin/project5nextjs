94578e3e98d774faf1986074d276b7e3d21c360c
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-23 10:45:46 +0100

    added staticSiteGeneration to navBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,8 +7,11 @@ function NavBar() {
   return (
     <div>
         <nav className='navigation-bar'>
-            <Link href='/'>home</Link> {} <Link href='/clientSideRender'>clientSideRender</Link> {} <Link href='/serverSideRender'>serverSideRender</Link>
-            {} <Link href='/product'>products</Link>
+            <Link href='/'>home</Link> {} 
+            <Link href='/clientSideRender'>clientSideRender</Link> {} 
+            <Link href='/serverSideRender'>serverSideRender</Link> {} 
+            <Link href='/staticSiteGeneration'>staticSiteGeneration</Link> {}
+            <Link href='/product'>products</Link>
             
         </nav>
     </div>
