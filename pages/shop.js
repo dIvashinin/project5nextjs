bd8e9fce122a05ab6b3adacd8f80436b0ce166cb
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-24 16:37:29 +0100

    trying to get data from my db and display it

--- pages/shop.js
+++ pages/shop.js
@@ -1,19 +1,41 @@
 import React from 'react'
 import { collection, getDocs } from "firebase/firestore"; 
 import { db } from '../config/firebaseConfig';
+import ProductCard from '../components/ProductCard';
 
-function Shop() {
+export const getStaticProps = async () => {
+    try {
+        const querySnapshot = await getDocs(collection(db, "products"));
+        const products = querySnapshot.forEach((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
 
-    const getProducts = async () => {
-const querySnapshot = await getDocs(collection(db, "products"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-  
-});
-}
+        return{
+            props: {products},
+            //will change this later, now it's 60 sec
+            // revalidate: 60
+        };
+
+    } catch (error) {
+        console.error('error fetching products:', error.message);
+        // return {
+        //     props: {products: []},
+        //     // revalidate: 60,
+        // };
+    }
+};
+
+function Shop({product}) {
+
   return (
     <div>
     <h2>shop</h2>
+    {products.map((product) => (
+        // {product.description}
+        <ProductCard key={product.description} product={product} />
+
+      ))}
     </div>
   );
 }
