5ffb1f450025e1eb0850e51ad12f4ed1fc3fa8a9
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-26 23:36:07 +0100

    trying to get data from my db and display it

--- pages/shop.js
+++ pages/shop.js
@@ -1,40 +1,37 @@
-import React from 'react'
-import { collection, getDocs } from "firebase/firestore"; 
-import { db } from '../config/firebaseConfig';
-import ProductCard from '../components/ProductCard';
+import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
+import ProductCard from "../components/ProductCard";
 
 export const getStaticProps = async () => {
-    try {
-        const querySnapshot = await getDocs(collection(db, "products"));
-        const products = querySnapshot.forEach((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
+  try {
+    const querySnapshot = await getDocs(collection(db, "products"));
+    const products = querySnapshot.forEach((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
 
-        return{
-            props: {products},
-            //will change this later, now it's 60 sec
-            // revalidate: 60
-        };
-
-    } catch (error) {
-        console.error('error fetching products:', error.message);
-        // return {
-        //     props: {products: []},
-        //     // revalidate: 60,
-        // };
-    }
+    return {
+      props: { products },
+      //will change this later, now it's 60 sec
+      // revalidate: 60
+    };
+  } catch (error) {
+    console.error("error fetching products:", error.message);
+    // return {
+    //     props: {products: []},
+    //     // revalidate: 60,
+    // };
+  }
 };
 
-function Shop({product}) {
-
+function Shop({ product }) {
   return (
     <div>
-    <h2>shop</h2>
-    {products.map((product) => (
+      <h2>shop</h2>
+      {products.map((product) => (
         // {product.description}
         <ProductCard key={product.description} product={product} />
-
       ))}
     </div>
   );
