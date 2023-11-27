aaef9570adc5d8899799e78010015124610f7d96
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-27 14:41:48 +0100

    reviews are on the screen

--- pages/shop.js
+++ pages/shop.js
@@ -6,39 +6,47 @@ import { db } from "../config/firebaseConfig";
 export const getStaticProps = async () => {
   try {
     const querySnapshot = await getDocs(collection(db, "reviews"));
-    const reviews = querySnapshot.forEach((doc) => {
+    const reviews = [];
+     querySnapshot.forEach((doc) => {
         //we can see our data in console!
         console.log(`${doc.id} => ${doc.data().review}`);
-        // id: doc.id,
+        reviews.push({
+            id: doc.id,
+            review: doc.data().review,
+        })
         // ...doc.data(),
     });
-    console.log('reviews :>> ', reviews);
+    // console.log('reviews :>> ', reviews);
 
     return {
       props: { reviews },
       //will change this later, now it's 60 sec
       // revalidate: 60
     };
-  } catch (error) {
+} catch (error) {
     console.error("error fetching products:", error.message);
-    // return {
-    //     props: {products: []},
-    //     // revalidate: 60,
-    // };
-  }
+    return {
+            props: {reviews: []},
+        //     // revalidate: 60,
+        };
+    }
 };
 
-function Shop({  }) {
+
+function Shop({ reviews }) {
   return (
     <div>
       <h2>shop</h2>
-      {/* {reviews.map((review) => ( */}
+      {reviews.map((review) => (
+        <div key={review.id}>
+            <p>{review.review}</p>
         {/* // {product.description} */}
         {/* // {review.name} */}
         {/* // <ProductCard key={review.description} review={review} /> */}
     {/* //   ))} */}
     </div>
-  );
+      ))};
+     </div> 
+  )
 }
-
 export default Shop;
