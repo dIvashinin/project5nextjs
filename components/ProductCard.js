a022c93e28a45b77fb7f2b5f36b6b3565578272f
dIvashinin <dmitry.ivashinin@gmail.com>
2023-11-27 16:32:34 +0100

    made a separate product card, styling it

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 function ProductCard({product}) {
   return (
-    <div>
-        <img src={product.image}></img>
-        {product.title}
-        {product.description}
-    </div>
+    //i don't use this one made for another fetch
+    // <div>
+    //     <img src={product.image}></img>
+    //     {product.title}
+    //     {product.description}
+    // </div>
+
+<>
+<img src={product.image}></img>
+{product.type}
+{product.price}
+{product.description}
+</>
+
   );
 }
 
-export default ProductCard
+export default ProductCard;
--- pages/shop.js
+++ pages/shop.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../config/firebaseConfig";
+import ProductCard from "../components/ProductCard";
 // import ProductCard from "../components/ProductCard";
 
 export const getStaticProps = async () => {
@@ -15,6 +16,7 @@ export const getStaticProps = async () => {
             type: doc.data().type,
             price: doc.data().price,
             description: doc.data().description,
+            image: doc.data().image,
         });
         console.log('products :>> ', products);
     });
@@ -56,11 +58,13 @@ function Shop({products, reviews }) {
         {/* here we display products */}
         <div className="products-shop">
             {products.map((product) => (
-                <div key = {product.id}>
+                <div key = {product.id} className="stuff-inside-products-div">
+                    <ProductCard product={product} />
+                    {/* <img className="product-image" src={product.image}></img>
                     <p>{product.type}</p>
                     <p>{product.price}</p>
-                    <p>{product.description}</p>
-                    {/* <p>{product.image}</p> */}
+                    <p>{product.description}</p> */}
+                    
                 </div>
             ))}
         </div>
