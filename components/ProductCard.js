ec44c9335ce0f9e6c8a9ec0e3bbc327a657b3827
dIvashinin <dmitry.ivashinin@gmail.com>
2023-12-01 15:29:21 +0100

    few small updates in product card component

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-function ProductCard({product}) {
+function ProductCard({ product }) {
   return (
     //i don't use this one made for another fetch
     // <div>
@@ -9,13 +9,16 @@ function ProductCard({product}) {
     //     {product.description}
     // </div>
 
-<>
-<img src={product.image}></img>
-<p data-tag="type">what: {product.type} { }</p>
-<p data-tag="price">price: {product.price}Eur { }</p>
-<p data-tag="description">{product.description}</p>
-</>
-
+    <>
+      <img src={product.image}></img>
+      <p data-tag="type">
+        what: {product.type} {}
+      </p>
+      <p data-tag="price">
+        price: {product.price}Eur {}
+      </p>
+      <p data-tag="description">{product.description}</p>
+    </>
   );
 }
 
