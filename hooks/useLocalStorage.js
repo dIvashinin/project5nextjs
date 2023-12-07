22ca3667ff518f0c060998d127f25e53d384d32c
dIvashinin <dmitry.ivashinin@gmail.com>
2023-12-07 18:18:11 +0100

    for now not use local storage, get back later

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -14,27 +14,60 @@ import { useEffect, useState } from "react";
 
 
 
-    export function useLocalStorage(key, initialValue) {
-        const [value, setValue] = useState(() => {
-          try {
-            const jsonValue = localStorage.getItem(key);
-            return jsonValue != null ? JSON.parse(jsonValue) : initialValue;
-          } catch (error) {
-            // Handle error or fallback to initialValue
-            console.error("Error accessing localStorage:", error);
-            return initialValue;
-          }
-        });
+    // export function useLocalStorage(key, initialValue) {
+    //     const isServer = typeof window === "undefined";
+    //     const [value, setValue] = useState(() => {
+    //       try {
+    //         const jsonValue = localStorage.getItem(key);
+    //         return jsonValue != null ? JSON.parse(jsonValue) : initialValue;
+    //       } catch (error) {
+    //         // Handle error or fallback to initialValue
+    //         console.error("Error accessing localStorage:", error);
+    //         return initialValue;
+    //       }
+    //     });
       
-        useEffect(() => {
-          try {
-            localStorage.setItem(key, JSON.stringify(value));
-          } catch (error) {
-            // Handle error
-            console.error("Error setting localStorage:", error);
-          }
-        }, [key, value]);
+    //     useEffect(() => {
+    //       try {
+    //         localStorage.setItem(key, JSON.stringify(value));
+    //       } catch (error) {
+    //         // Handle error
+    //         console.error("Error setting localStorage:", error);
+    //       }
+    //     }, [key, value]);
       
-        return [value, setValue];
+    //     return [value, setValue];
+    //   }
+
+    //   import { useEffect, useState } from "react";
+
+export function useLocalStorage(key, initialValue) {
+  const isServer = typeof window === "undefined";
+
+  const [value, setValue] = useState(() => {
+    try {
+      if (!isServer) {
+        const jsonValue = localStorage.getItem(key);
+        return jsonValue != null ? JSON.parse(jsonValue) : initialValue;
       }
+    } catch (error) {
+      console.error("Error accessing localStorage:", error);
+    }
+
+    return initialValue;
+  });
+
+  useEffect(() => {
+    try {
+      if (!isServer) {
+        localStorage.setItem(key, JSON.stringify(value));
+      }
+    } catch (error) {
+      console.error("Error setting localStorage:", error);
+    }
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
 // }
