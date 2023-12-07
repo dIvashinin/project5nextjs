7bc9ed524c26313b87fb605bb9ba62adf5450ff7
dIvashinin <dmitry.ivashinin@gmail.com>
2023-12-07 22:53:48 +0100

    modifying about page

--- pages/about.js
+++ pages/about.js
@@ -1,32 +1,61 @@
-import React from 'react'
+import React from "react";
 
 function about() {
   return (
     <div>
-        <h2>About this shop</h2>
-    <p>
-    Jewelry winning over anxiety
-Handmade anti-anxiety jewelry made in Berlin from natural stones and noble metals.</p>
+      <h2>About this shop</h2>
+        <h3>Jewelry winning over anxiety</h3>
+      <p>
+        Handmade anti-anxiety jewelry made in
+        Berlin from natural stones and noble metals.
+      </p>
 
-<p>Hi! I've been living with anxiety for quite a long time, but founding my new passion in jewelry making helped me so much ever since. Working with the natural stones, hearing them clacking, seeing the light playing through them on my skin and doing some breathing exercises while I am at it - I found it super soothing.
-I wear my jewelry 24/7 and noticed, that fidgeting and playing with it really helps me against anxiety symptoms and raises my concentration. I hope you' feel the same and will love your new pieces!
-I came up with my first anti-anxiety fidget ring back in 2019 and since then I am constantly trying to invent and create new designs - that are helpful and cute! As my craftsmanship grows, I'm becoming able to create more elaborate jewelry types, but the idea behind this is always the same - helping people with anxiety (like myself) to feel better through playful and pretty jewelry designs. Oh, and I named the store in honour of my cat Ruby (RIP my beautiful fur baby)
-xx Love, Oksana</p>
+      <p>
+        Hi! I've been living with anxiety for quite a long time, but founding my
+        new passion in jewelry making helped me so much ever since. Working with
+        the natural stones, hearing them clacking, seeing the light playing
+        through them on my skin and doing some breathing exercises while I am at
+        it - I found it super soothing. I wear my jewelry 24/7 and noticed, that
+        fidgeting and playing with it really helps me against anxiety symptoms
+        and raises my concentration. I hope you' feel the same and will love
+        your new pieces! I came up with my first anti-anxiety fidget ring back
+        in 2019 and since then I am constantly trying to invent and create new
+        designs - that are helpful and cute! As my craftsmanship grows, I'm
+        becoming able to create more elaborate jewelry types, but the idea
+        behind this is always the same - helping people with anxiety (like
+        myself) to feel better through playful and pretty jewelry designs. Oh,
+        and I named the store in honour of my cat Ruby (RIP my beautiful fur
+        baby) xx Love, Oksana
+      </p>
 
-<p>P.S. Pls follow the shop on Insta
-https://www.instagram.com/moonrubyshop/</p>
+      <p>
+        P.S. Pls follow the shop on Insta
+        https://www.instagram.com/moonrubyshop/
+      </p>
 
-<p>Why do I call my jewelry anti-anxiety?🤍
+      <p>
+        Why do I call my jewelry anti-anxiety?🤍 😌 All the jewellery is perfect
+        for fidgeting, especially the rings. I found that playing with the stone
+        beads helps me to concentrate and relax, especially in stressful
+        situations. I love how gems are changing their temperature, how they
+        clack, how the light breaks in them, how unique each one of them looks.
+      </p>
 
-😌All the jewellery is perfect for fidgeting, especially the rings. I found that playing with the stone beads helps me to concentrate and relax, especially in stressful situations. I love how gems are changing their temperature, how they clack, how the light breaks in them, how unique each one of them looks.</p>
+      <p>
+        💆🏻‍♀️ I choose natural gemstones known by their anti-anxiety properties:
+        like amethyst, rose quartz, aventurine etc. I believe that everything
+        coming from nature has a little bit of magic in it and is able so store
+        vibes and energy.
+      </p>
 
-<p>💆🏻‍♀️I choose natural gemstones known by their anti-anxiety properties: like amethyst, rose quartz, aventurine etc. I believe that everything coming from nature has a little bit of magic in it and is able so store vibes and energy.</p>
-
-<p>🤗Speaking of vibes, making jewelry helps me a lot as well. It’s a bit like meditation, helps me to look inwards and to feel calmer generally. So I make my pieces with very positive energy, trying to keep the process stress free.</p>
-    
-        
+      <p>
+        🤗 Speaking of vibes, making jewelry helps me a lot as well. It’s a bit
+        like meditation, helps me to look inwards and to feel calmer generally.
+        So I make my pieces with very positive energy, trying to keep the
+        process stress free.
+      </p>
     </div>
-  )
+  );
 }
 
-export default about
+export default about;
