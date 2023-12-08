9c4f1c5459ef8c773a8d028adf669b2a3324f6d4
dIvashinin <dmitry.ivashinin@gmail.com>
2023-12-08 20:24:19 +0100

    thinking of single product page and shop policies

--- components/Footer.js
+++ components/Footer.js
@@ -8,6 +8,8 @@ const etsyIcon =
   "https://res.cloudinary.com/dzghua4dz/image/upload/v1701980006/moonrubyshop/h8z81huv3zccge5a5gks.svg";
 const instagramIcon =
   "https://res.cloudinary.com/dzghua4dz/image/upload/v1701979993/moonrubyshop/waqgyccu5yktbke9tm6a.svg";
+const FAQ = 
+  "https://res.cloudinary.com/dzghua4dz/image/upload/v1702062726/moonrubyshop/iaiimnkwoj5kuzckba0v.svg";
 
 function Footer() {
   return (
@@ -30,6 +32,15 @@ function Footer() {
             style={{ width: "30px", height: "30px" }}
           />
           {/* <FontAwesomeIcon icon={faInstagram} size="2x" /> */}
+        </a>
+      </Link>
+      <Link href="https://www.instagram.com/moonrubyshop/" passHref>
+        <a target="_blank" rel="noopener noreferrer">
+          <img
+            src={FAQ}
+            alt="FAQ"
+            style={{ width: "30px", height: "30px" }}
+          />
         </a>
       </Link>
     </div>
