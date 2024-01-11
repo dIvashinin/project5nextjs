c85df5fa388bd8032dc847aae8e6f4b3da1dca7b
dIvashinin <dmitry.ivashinin@gmail.com>
2024-01-11 18:59:17 +0100

    styling of success page

--- pages/success.js
+++ pages/success.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function success() {
   return (
-    <div>successfull success! You did it</div>
+    <div className='success-container'>
+        <h2 className="moon-ruby-shop">Thanks and hugs from Moon Ruby Shop</h2>
+        <h2>successfull success! You did it</h2>
+        <h3>check your e-mail with order details</h3>
+        </div>
   )
 }
 
