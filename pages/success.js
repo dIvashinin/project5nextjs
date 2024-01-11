6ab0ccf0f06e89631fb85281795860e83b702a91
dIvashinin <dmitry.ivashinin@gmail.com>
2024-01-11 19:34:44 +0100

    styling of success page

--- pages/success.js
+++ pages/success.js
@@ -4,8 +4,9 @@ function success() {
   return (
     <div className='success-container'>
         <h2 className="moon-ruby-shop">Thanks and hugs from Moon Ruby Shop</h2>
-        <h2>successfull success! You did it</h2>
-        <h3>check your e-mail with order details</h3>
+        <h2>You did it</h2>
+        <h2>successfull success!</h2>
+        <h4>check your e-mail with order details</h4>
         </div>
   )
 }
