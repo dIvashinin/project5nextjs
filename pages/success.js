8db7bdd8c472891819ea38efc74707282d18626c
dIvashinin <dmitry.ivashinin@gmail.com>
2024-01-13 22:04:46 +0100

    adding an email confirmation util, updating success page

--- pages/success.js
+++ pages/success.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React from 'react';
+// Import necessary dependencies and the sendConfirmationEmail function
+import { useEffect } from 'react';
+import { sendConfirmationEmail } from '/utils/emailUtil';
 
-function success() {
+function success(docRef) {
+
+    // Trigger sendConfirmationEmail on component mount
+  useEffect(() => {
+    const orderDetails = docRef.id;
+    const toEmail = docRef.id;
+    sendConfirmationEmail(toEmail, orderDetails);
+  }, []); // Empty dependency array ensures the effect runs only once after mount
+
   return (
     <div className='success-container'>
         <h2 className="moon-ruby-shop">Thanks and hugs from Moon Ruby Shop</h2>
