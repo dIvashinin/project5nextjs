98e2371807a8800223859e672f422e106c42b53f
dIvashinin <dmitry.ivashinin@gmail.com>
2024-01-17 18:27:58 +0100

    modified and styled custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 //we can create for every error this kind of pages
+const notFoundPageBackground = "https://res.cloudinary.com/dzghua4dz/image/upload/v1705325724/moonrubyshop/nygvgkwzp5zvfijgvmbo.jpg"
 
 function NotFoundPage() {
   return (
-    <h1>this is our custom 404 page</h1>
+    <div className='not-found-container'>
+      <div className='not-found-background-container'>
+      <img src={notFoundPageBackground} alt="Etsy" className="not-found-image" />
+      </div>
+    <h2>it seems like there's an oops happening</h2>
+    <h2>probably this page doesn't exist at all</h2>
+    </div>
     )
 };
 
