25a900f1aeb253e41db889298b8724937e408758
dIvashinin <dmitry.ivashinin@gmail.com>
2024-01-19 21:26:18 +0100

    dashboard - env is ok, redirect to dashboard works, still some issues

--- components/ProtectedRoute.js
+++ components/ProtectedRoute.js
@@ -9,39 +9,83 @@ import { auth } from '../config/firebaseConfig';
 //make sure it's prefixed with NEXT_PUBLIC_
 const SHOP_OWNER_UID=process.env.NEXT_PUBLIC_USER_UID;
 
-// const SHOP_OWNER_UID ='VRsq623ztSPYtY0xr7quFv8Heyx2';
-// console.log('SHOP_OWNER_UID :>> ', SHOP_OWNER_UID);
-
 const ProtectedRoute = ({children}) => {
 const [user, setUser] = useState(null);
 const router = useRouter();
-// console.log('user :>> ', user);
-// console.log('user.uid :>> ', user.uid);
-useEffect(() => {
-  const unsubscribe = onAuthStateChanged (auth, (user) =>{
-    if (user && user.uid === SHOP_OWNER_UID) {
-        setUser(user);
-        // console.log('hi shop owner! welcome!');
-        // Redirect to the dashboard when the correct user is logged in
-        router.replace('/dashboard');
-        
-    } else {
-        // redirect (in login case 'replace', because we replace the prev link and can't go back)
-        router.replace('/login');
-    }
-});
-return () => unsubscribe();
-}, [router]);
 
-if (!user) {
-    //here loading spinner later
-    return null;
-    //i was returning null here and it was complaining about it
-    // so i change to 'nothing'
-    // return;
-}
+// useEffect(() => {
+//   const unsubscribe = onAuthStateChanged (auth, (user) =>{
+//     if (user && user.uid === SHOP_OWNER_UID) {
+//         setUser(user);
+//         // console.log('hi shop owner! welcome!');
+//         // Redirect to the dashboard when the correct user is logged in
+//         router.replace('/dashboard');
+        
+//     } if (!user) {
+//         // redirect (in login case 'replace', because we replace the prev link and can't go back)
+//         router.replace('/login');
+//     }
+// });
+// if (!user) {
+//     console.log('restricted area');
+//     //here loading spinner later
+//     return;
+//     //i was returning null here and it was complaining about it
+//     // so i change to 'nothing'
+//     // return;
+// }
+// return () => unsubscribe();
+// }, [router]);
+
   
-return <>{children}</>;
+// return <>{children}</>;
+
+
+
+// const checkIfUserIsShopOwner = () => {
+//     onAuthStateChanged(auth, (user) => {
+//       if (user && user.uid === SHOP_OWNER_UID) {
+//         setUser(user);
+//         // console.log("hey user!");
+//         router.replace('/dashboard');
+//       } else {
+//         //   console.log("you are not the user!");
+//         //   setUser(null);
+//           router.replace('/login');
+//       }
+//     });
+//   };
+// //   checkIfUserIsShopOwner ();
+
+// };
+
+
+// export default ProtectedRoute;
+
+useEffect(() => {
+    const checkIfUserIsShopOwner = () => {
+      onAuthStateChanged(auth, (authUser) => {
+        if (authUser && authUser.uid === SHOP_OWNER_UID) {
+          setUser(authUser);
+          router.replace('/dashboard');
+        } else {
+        //   setUser(null);
+        console.log('no!');
+          router.replace('/login');
+        }
+      });
+    };
+
+    checkIfUserIsShopOwner();
+  }, [router]);
+
+  if (!user) {
+    // Loading spinner or any other loading indication can be placed here
+    return null;
+    
+  }
+
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
