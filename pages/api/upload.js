d41107b3d944ef58ca846bea3c428bd2a3f0fb7c
dIvashinin <dmitry.ivashinin@gmail.com>
2024-01-30 19:31:02 +0100

    started creating upload via cloudinary

---
+++ pages/api/upload.js
@@ -0,0 +1,19 @@
+import cloudinary from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+  export default async function handleUpload (req, res) {
+    if (req.method === 'POST') {
+        const result = await cloudinary.v2.uploader.upload(req.body.image, {
+          folder: 'moonrubyshop', // specify a folder in Cloudinary
+        });
+    
+        res.status(200).json(result);
+      } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
+      }
+    }
