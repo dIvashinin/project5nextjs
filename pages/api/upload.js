c5a5ca8ed5e78c2ed3ebae2471e2bac6817ff432
dIvashinin <dmitry.ivashinin@gmail.com>
2024-02-06 20:51:51 +0100

    trying to implement cloudinary, inputs are ok, upload is back, wait for the issues

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,20 +1,21 @@
-// import cloudinary from "cloudinary";
+import {v2 as cloudinary} from "cloudinary";
 
-// cloudinary.config({
-//   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
 
-// export default async function handleUpload(req, res) {
-//   if (req.method === "POST") {
-//     const result = await cloudinary.v2.uploader.upload(req.body.image, {
-//       folder: "moonrubyshop", // specify a folder in Cloudinary
-//     });
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-//     res.status(200).json(result);
-//     console.log("upload success");
-//   } else {
-//     res.status(405).json({ message: "Method Not Allowed" });
-//   }
-// }
+export default async function handleUpload(req, res) {
+  if (req.method === "POST") {
+    const result = await cloudinary.uploader.upload(req.body.image, {
+      folder: "moonrubyshop", // specify a folder in Cloudinary
+    });
+
+    res.status(200).json(result);
+    console.log("upload success");
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
