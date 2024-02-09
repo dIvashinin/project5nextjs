6a2eff585bfca652e892e0c799905aafd08e9aaf
dIvashinin <dmitry.ivashinin@gmail.com>
2024-02-09 23:31:38 +0100

    config multer

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,9 +1,21 @@
 import { v2 as cloudinary } from "cloudinary";
+import multer from 'multer';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Define storage options
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Specify the destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Use the original file name for the uploaded file
+  }
 });
 
 export default async function handleUpload(req, res) {
