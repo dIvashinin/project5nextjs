104be52eb2f7b121880578d84c5949e1389bfdc1
dIvashinin <dmitry.ivashinin@gmail.com>
2024-02-12 14:20:25 +0100

    trying to use client-side img upload

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -38,6 +38,34 @@ const handleImageChange = (e) => {
   setImage(e.target.files[0]);
 };
 
+//taken from https://cloudinary.com/documentation/client_side_uploading
+const url = "https://api.cloudinary.com/v1_1/process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME/image/upload";
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const files = document.querySelector("[type=file]").files;
+  const formData = new FormData();
+
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    formData.append("file", file);
+    formData.append("upload_preset", "docs_upload_example_us_preset");
+
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        document.getElementById("data").innerHTML += data;
+      });
+  }
+});
+
 
 // useEffect(() => {
 //   const fetchOrders = async () => {
