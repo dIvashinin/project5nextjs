fd9b79d65eaa1adedcc148aff7b383ab9c1df1ad
dIvashinin <dmitry.ivashinin@gmail.com>
2024-02-22 21:15:13 +0100

    created DELETE in backend

--- pages/api/[productId].js
+++ pages/api/[productId].js
@@ -1,4 +1,4 @@
-import { doc, updateDoc, collection } from "firebase/firestore";
+import { doc, updateDoc, deleteDoc, collection } from "firebase/firestore";
 import { db } from "../../config/firebaseConfig";
 
 export default async function handler(req, res) {
@@ -27,7 +27,20 @@ export default async function handler(req, res) {
         console.error('Error updating product:', error);
         res.status(500).json({ error: 'Failed to update product' });
       }
+    } else if (req.method === 'DELETE') {
+
+    try {
+        //creating reference to the specific doc using doc() function
+        const productDocRef = doc(db, 'products', productId);
+        //deleting doc
+        await deleteDoc(productDocRef);
+        res.status(200).json({message: 'Product deleted'});
+    } catch (error) {
+        console.error('Error deleting product:', error);
+        res.status(500).json({error: 'Failed to delete product'});
+    }
     } else {
         res.status(405).json({ error: 'Method Not Allowed' }); // Respond with Method Not Allowed error for other HTTP methods
     }
 }
+
