628fc4718a29cb5e766b107a158eaf799874c98b
dIvashinin <dmitry.ivashinin@gmail.com>
2024-03-12 20:04:43 +0100

    now all images are uploaded to cloudinary, db and fetched, add alerts

--- pages/success.js
+++ pages/success.js
@@ -1,23 +1,24 @@
 // import { doc, updateDoc } from 'firebase/firestore';
-import React, { useEffect }from 'react';
+import React, { useEffect } from "react";
 // import { db } from '../config/firebaseConfig';
 // import { useShoppingCart } from '../context/shoppingCartContext';
 // Import necessary dependencies and the sendConfirmationEmail function
 // import { useEffect } from 'react';
 // import { useRouter } from 'next/router';
 // import { sendConfirmationEmail } from '/utils/emailUtil';
-const successBackground = "https://res.cloudinary.com/dzghua4dz/image/upload/v1705325724/moonrubyshop/nygvgkwzp5zvfijgvmbo.jpg"
+const successBackground =
+  "https://res.cloudinary.com/dzghua4dz/image/upload/v1705325724/moonrubyshop/nygvgkwzp5zvfijgvmbo.jpg";
 
 function Success() {
-//   const Success = ({ orderId, totalSum }) => {
-//     useEffect (() => {
-//       const updateOrder = async () => {
-//   // Assuming you have the order ID
-// const orderDocRef = doc (db, "orders", orderId);
-// await updateDoc(orderDocRef, { paid: true });
-// };
-// updateOrder();
-//     }, [orderId]);
+  //   const Success = ({ orderId, totalSum }) => {
+  //     useEffect (() => {
+  //       const updateOrder = async () => {
+  //   // Assuming you have the order ID
+  // const orderDocRef = doc (db, "orders", orderId);
+  // await updateDoc(orderDocRef, { paid: true });
+  // };
+  // updateOrder();
+  //     }, [orderId]);
 
   // const {cartItems, setCartItems} = useShoppingCart();
   // const router = useRouter();
@@ -54,15 +55,15 @@ function Success() {
   // }, []); // Empty dependency array ensures the effect runs only once after mount
 
   return (
-    <div className='success-container'>
+    <div className="success-container">
       <div className="success-background-container">
         <img src={successBackground} alt="Success" className="success-image" />
       </div>
-        <h2 className="moon-ruby-shop">Thanks and hugs from Moon Ruby Shop</h2>
-        <h2>You did it</h2>
-        <h2>successfull success!</h2>
-        <h4>check your e-mail with order details</h4>
-        </div>
+      <h2 className="moon-ruby-shop">Thanks and hugs from Moon Ruby Shop</h2>
+      <h2>You did it</h2>
+      <h2>successfull success!</h2>
+      <h4>check your e-mail with order details</h4>
+    </div>
   );
 }
 export default Success;
