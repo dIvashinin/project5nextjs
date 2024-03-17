1b20373f7f3940f6556334523d9f0d5fd13d5464
dIvashinin <dmitry.ivashinin@gmail.com>
2024-03-17 20:58:49 +0100

    adding to cloudinary/ update of existing images by 1 image works

--- pages/api/[productId].js
+++ pages/api/[productId].js
@@ -4,18 +4,21 @@ import { doc, updateDoc, deleteDoc, collection } from "firebase/firestore";
 import { db } from "../../config/firebaseConfig";
 
 export default async function handler(req, res) {
-    const { productId } = req.query;
-  
-    if (req.method === 'PUT') {
-      try {
-        const { type, price, description
-          // i exclude image because i do add image separately
-          // , image
-         } = req.body;
+  const { productId } = req.query;
+
+  if (req.method === "PUT") {
+    try {
+      const {
+        type,
+        price,
+        description,
+        // i exclude image because i do add image separately
+        // , image
+      } = req.body;
       console.log("type :>> ", type);
 
-        // Create a reference to the specific document using doc() function
-        const productDocRef = doc(db, "products2", productId);
+      // Create a reference to the specific document using doc() function
+      const productDocRef = doc(db, "products2", productId);
 
       // Update the document with the specified data using updateDoc() function
       await updateDoc(productDocRef, {
@@ -24,16 +27,16 @@ export default async function handler(req, res) {
         description,
         // image,
         // Add other fields as needed
-    });
+      });
 
-    res.status(200).json({ message: 'Product successfully updated' });
-    console.log('success!');
-      }catch (error) {
-        console.error('Error updating product:', error);
-        res.status(500).json({ error: 'Failed to update product' });
-      }
+      res.status(200).json({ message: "Product successfully updated" });
+      console.log("success!");
+    } catch (error) {
+      console.error("Error updating product:", error);
+      res.status(500).json({ error: "Failed to update product" });
+    }
 
-//it's a common practice to have multiple handler fuctions within one file for different types of requests 
+    //it's a common practice to have multiple handler fuctions within one file for different types of requests
 
     // } else if (req.method === 'PATCH') { // Using PATCH method for updating the image
     //   try {
@@ -49,25 +52,22 @@ export default async function handler(req, res) {
     //     } catch (error) {
     //       console.error('Error updating product image:', error);
     //       res.status(500).json({ error: 'Failed to update product image' });
-    //   }  
+    //   }
 
     //it's a common practice to have multiple handler fuctions within one file for different types of requests
-    
-        } else if (req.method === 'DELETE') {
-
+  } else if (req.method === "DELETE") {
     try {
-        //creating reference to the specific doc using doc() function
-        const productDocRef = doc(db, 'products', productId);
-        //deleting doc
-        await deleteDoc(productDocRef);
-        res.status(200).json({message: 'Product deleted'});
-        console.log('deleted!!!!');
+      //creating reference to the specific doc using doc() function
+      const productDocRef = doc(db, "products", productId);
+      //deleting doc
+      await deleteDoc(productDocRef);
+      res.status(200).json({ message: "Product deleted" });
+      console.log("deleted!!!!");
     } catch (error) {
-        console.error('Error deleting product:', error);
-        res.status(500).json({error: 'Failed to delete product'});
+      console.error("Error deleting product:", error);
+      res.status(500).json({ error: "Failed to delete product" });
     }
-    } else {
-        res.status(405).json({ error: 'Method Not Allowed' }); // Respond with Method Not Allowed error for other HTTP methods
-    }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" }); // Respond with Method Not Allowed error for other HTTP methods
+  }
 }
-
