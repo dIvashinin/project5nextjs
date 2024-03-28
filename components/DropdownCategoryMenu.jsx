cca24d32c4861170916206c26ce0b89b2cf651ec
dIvashinin <dmitry.ivashinin@gmail.com>
2024-03-28 22:26:31 +0100

    unique categories are in off canvas, retrieved from firestore db and fetched

--- components/DropdownCategoryMenu.jsx
+++ components/DropdownCategoryMenu.jsx
@@ -2,6 +2,7 @@ import { Offcanvas, Stack } from "react-bootstrap";
 import React, { useEffect, useState } from 'react'
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { db } from "../config/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
 function DropdownCategoryMenu({isOpen2}) {
     // const [isOpen2, setIsOpen2] = useState(false);
@@ -17,11 +18,10 @@ function DropdownCategoryMenu({isOpen2}) {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-      first
     
         const fetchCategories = async () => {
             try {
-                const querySnapshot = await db.collection('products2').get();
+                const querySnapshot = await getDocs(collection(db, 'products2'));
                 const uniqueCategories = new Set();
                 querySnapshot.forEach((doc) => {
                     const productData = doc.data();
@@ -54,6 +54,9 @@ function DropdownCategoryMenu({isOpen2}) {
       {/* in order to have our added to cart items visible */}
       <Offcanvas.Body>
         <Stack gap={1}>
+        {categories.map((category) => (
+              <div key={category}>{category}</div>
+            ))}
         </Stack>
       </Offcanvas.Body>
       </Offcanvas>
