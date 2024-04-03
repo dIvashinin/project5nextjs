8ddb0a9fe829e2b93c40a974477d767f6f8e3717
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-03 16:42:30 +0200

    fixing redirection to filteredProducts when outside of shop page, create a custom hook

---
+++ hooks/useRedirectOnCondition.js
@@ -0,0 +1,4 @@
+// this is my custom hook for redirecting user if he decides to use
+// dropdown offcanvas when inside other than shop/index page
+// this approach helps me keep my code DRY = DON'T REPEAT YOURSELF
+
