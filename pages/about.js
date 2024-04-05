c618650a9e62b0088e12573efbec0c6b71460a92
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-05 22:47:16 +0200

    a few text and css updates in about

--- pages/about.js
+++ pages/about.js
@@ -20,33 +20,34 @@ function About() {
       <div className="about-main-container">
       <h2 data-tag="about">About this shop</h2>
         <h3 data-tag="title">Jewelry winning over anxiety</h3>
-      <p>
-        Handmade anti-anxiety jewelry made in
-        Berlin from natural stones and noble metals.
-      </p>
-
+      <h6 data-tag="title">
+        Handmade anti-anxiety jewelry made in Berlin from natural stones and noble metals
+      </h6>
       <p>
         Hi! I&apos;ve been living with anxiety for quite a long time, but founding my
         new passion in jewelry making helped me so much ever since. Working with
         the natural stones, hearing them clacking, seeing the light playing
         through them on my skin and doing some breathing exercises while I am at
-        it - I found it super soothing. I wear my jewelry 24/7 and noticed, that
+        it - I found it super soothing. </p>
+        
+        <p>I wear my jewelry 24/7 and noticed, that
         fidgeting and playing with it really helps me against anxiety symptoms
         and raises my concentration. I hope you&apos;ll feel the same and will love
         your new pieces! I came up with my first anti-anxiety fidget ring back
         in 2019 and since then I am constantly trying to invent and create new
-        designs - that are helpful and cute! As my craftsmanship grows, I&apos;m
+        designs - that are helpful and cute! </p> 
+        
+        <p>As my craftsmanship grows, I&apos;m
         becoming able to create more elaborate jewelry types, but the idea
         behind this is always the same - helping people with anxiety (like
         myself) to feel better through playful and pretty jewelry designs. Oh,
         and I named the store in honour of my cat Ruby (RIP my beautiful fur
         baby) xx Love, Oksana
       </p>
-
-      <p>
+      {/* <p>
         P.S. Pls follow the shop on Insta
         https://www.instagram.com/moonrubyshop/
-      </p>
+      </p> */}
 
       <p>
         Why do I call my jewelry anti-anxiety? 😌 All the jewellery is perfect
