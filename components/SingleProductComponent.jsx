d2d341c2badc18843a853195911e1160e214ba98
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-15 21:26:54 +0200

    added faq page

--- components/SingleProductComponent.jsx
+++ components/SingleProductComponent.jsx
@@ -485,7 +485,7 @@ function SingleProductCard({ product }) {
               {/* <option disabled value="pls select color">pls select color</option> */}
               <option value="option 1">option 1</option>
               <option value="option 2">option 2</option>
-              <option value="option 3">option 3</option>
+              {/* <option value="option 3">option 3</option> */}
               {/* <option value="option 4">option 4</option> */}
               {/* <option value="option 5">option 5</option> */}
               {/* <option value="option 6">option 6</option> */}
