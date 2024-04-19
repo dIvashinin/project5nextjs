4481a7d71e1dfd8ae9eaa8e73a0ccefdccda85d2
dIvashinin <dmitry.ivashinin@gmail.com>
2024-04-19 21:28:27 +0200

    fixed banner in success space between

--- pages/success.js
+++ pages/success.js
@@ -60,10 +60,10 @@ function Success() {
       <div className="success-background-container">
         <img src={successBackground} alt="Success" className="success-image" />
       </div>
-      <h2 className="moon-ruby-shop">Thanks and hugs from Moon Ruby Shop</h2>
+      <h2 className="thanks-and-hugs">Thanks and hugs from Moon Ruby Shop</h2>
       {/* <h2>You did it</h2> */}
       {/* <h2>successfull success!</h2> */}
-      <h4>check your e-mail with order details</h4>
+      <h4 className="check-your-email">check your e-mail with order details</h4>
     </div>
   );
 }
